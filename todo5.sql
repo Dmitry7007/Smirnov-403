3d973cd0566b609d54275c1fe3c0ac10a4fb690e
Dmitry7007 <99890690+Dmitry7007@users.noreply.github.com>
2022-04-04 12:14:15 +0700

    Delete todo5.sql

--- todo5.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE users add COLUMN age TINYINT(3);
-UPDATE users SET age = (YEAR(CURRENT_DATE)-YEAR(birthdate))-(RIGHT(CURRENT_DATE,5)<RIGHT(birthdate,5));
-SELECT MAX(age) as 'minimal age' FROM users;
-SELECT MIN(age) as 'maximal age' FROM users;
-SELECT AVG(age) as 'average age' FROM users;
-
